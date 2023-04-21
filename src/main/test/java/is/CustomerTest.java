ee89afc5b36a5f7e84c902926855f6888ff0847a
villimani <vilmundurmani@gmail.com>
2023-04-21 17:56:23 +0000

    Byrjaði að búa til test

---
+++ src/main/test/java/is/CustomerTest.java
@@ -0,0 +1,17 @@
+package is;
+
+import is.vinnsla.Customer;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CustomerTest {
+    @Test
+    public void testCustomer() {
+        Customer c1 = new Customer("John", "123 Main Street");
+        assertEquals("John", c1.getName());
+        assertEquals("123 Main Street", c1.getAddress());
+    }
+}
+
---
+++ src/main/test/java/is/MealsTest.java
@@ -0,0 +1,15 @@
+package is;
+
+import is.vinnsla.Meals;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MealsTest {
+    @Test
+    public void testMeals() {
+        Meals meal=new Meals("Pizza",1500);
+        assertEquals(1500,meal.getPrice());
+
+    }
+}
